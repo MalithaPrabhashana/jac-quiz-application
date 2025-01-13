ab9ac03c28e6efd68a4a71d2d77c06e6b861c468
GayuruRamanayake <rpgkr3@gmail.com>
2025-01-13 13:55:46 +0530

    update

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,8 +2,9 @@ import { MantineProvider } from '@mantine/core';
 import Login from './components/Auth/Login'; // Adjust the path as needed
 import '@mantine/core/styles.css';
 import Signup from './components/Auth/Signup';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Dashboard from './pages/Dashboard';
+
 // import './App.css';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
     <MantineProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} /> 
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
