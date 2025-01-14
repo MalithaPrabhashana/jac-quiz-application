52bb9b87477fd8d21b25ade8932a33b3991dbe3d
GayuruRamanayake <rpgkr3@gmail.com>
2025-01-15 01:17:37 +0530

    update

--- FE/src/pages/student/StudentDashboard.tsx
+++ FE/src/pages/student/StudentDashboard.tsx
@@ -1,57 +1,78 @@
 import { Container, Text, Button, Paper, Group } from '@mantine/core';
+import Sidebar from '../../components/Sidebar';
 
 const StudentDashboard = () => {
   return (
-    <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 40 }}>
-      {/* Welcome section */}
-      <Container style={{ textAlign: 'center', marginBottom: 40 }}>
-        <Text size="xl" style={{ marginBottom: 8 }}>
-          Welcome back, Student!
-        </Text>
-        <Text color="dimmed" size="sm">
-          Access your quizzes, results, and courses from here.
-        </Text>
-      </Container>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      
+      {/* Sidebar */}
+      <div style={{ width: '250px', height: '100vh', position: 'fixed', top: 0, left: 0, zIndex: 10 }}>
+        <Sidebar />
+      </div>
 
-      {/* Take Quiz */}
-      <Paper radius="md" shadow="sm" withBorder style={{ padding: 30, width: '100%', maxWidth: 500, marginBottom: 20 }}>
-        <Text size="lg" style={{ marginBottom: 16 }}>
-          Take a Quiz
-        </Text>
-        <Text size="sm" color="dimmed" style={{ marginBottom: 20 }}>
-          Start a new quiz to test your knowledge.
-        </Text>
-        <Group style={{ justifyContent: 'center' }}>
-          <Button variant="outline">Start Quiz</Button>
-        </Group>
-      </Paper>
+      {/* Main Content */}
+      <div
+        style={{
+          flex: 1,
+          marginLeft: '250px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '20px',
+        }}
+      >
+        <Container style={{ width: '100%', maxWidth: '600px', textAlign: 'center' }}>
+          
+          {/* Welcome Section */}
+          <Text size="xl" style={{ marginBottom: 16, fontWeight: 600 }}>
+            Welcome back, Student!
+          </Text>
+          <Text color="dimmed" size="sm" style={{ marginBottom: 40 }}>
+            Access your quizzes, results, and courses from here.
+          </Text>
 
-      {/* View Results */}
-      <Paper radius="md" shadow="sm" withBorder style={{ padding: 30, width: '100%', maxWidth: 500, marginBottom: 20 }}>
-        <Text size="lg" style={{ marginBottom: 16 }}>
-          View Results
-        </Text>
-        <Text size="sm" color="dimmed" style={{ marginBottom: 20 }}>
-          Check your quiz performance and progress.
-        </Text>
-        <Group style={{ justifyContent: 'center' }}>
-          <Button variant="outline">View Results</Button>
-        </Group>
-      </Paper>
+          {/* Take Quiz */}
+          <Paper radius="md" shadow="sm" withBorder style={{ padding: 30, marginBottom: 20 }}>
+            <Text size="lg" style={{ marginBottom: 16 }}>
+              Take a Quiz
+            </Text>
+            <Text size="sm" color="dimmed" style={{ marginBottom: 20 }}>
+              Start a new quiz to test your knowledge.
+            </Text>
+            <Group align="center" justify="center">
+              <Button variant="outline">Start Quiz</Button>
+            </Group>
+          </Paper>
 
-      {/* Enrolled Courses */}
-      <Paper radius="md" shadow="sm" withBorder style={{ padding: 30, width: '100%', maxWidth: 500, marginBottom: 20 }}>
-        <Text size="lg" style={{ marginBottom: 16 }}>
-          Enrolled Courses
-        </Text>
-        <Text size="sm" color="dimmed" style={{ marginBottom: 20 }}>
-          View all the courses you are enrolled in.
-        </Text>
-        <Group style={{ justifyContent: 'center' }}>
-          <Button variant="outline">View Courses</Button>
-        </Group>
-      </Paper>
-    </Container>
+          {/* View Results */}
+          <Paper radius="md" shadow="sm" withBorder style={{ padding: 30, marginBottom: 20 }}>
+            <Text size="lg" style={{ marginBottom: 16 }}>
+              View Results
+            </Text>
+            <Text size="sm" color="dimmed" style={{ marginBottom: 20 }}>
+              Check your quiz performance and progress.
+            </Text>
+            <Group align="center" justify="center">
+              <Button variant="outline">View Results</Button>
+            </Group>
+          </Paper>
+
+          {/* Enrolled Courses */}
+          <Paper radius="md" shadow="sm" withBorder style={{ padding: 30 }}>
+            <Text size="lg" style={{ marginBottom: 16 }}>
+              Enrolled Courses
+            </Text>
+            <Text size="sm" color="dimmed" style={{ marginBottom: 20 }}>
+              View all the courses you are enrolled in.
+            </Text>
+            <Group align="center" justify="center">
+              <Button variant="outline">View Courses</Button>
+            </Group>
+          </Paper>
+
+        </Container>
+      </div>
+    </div>
   );
 };
 
